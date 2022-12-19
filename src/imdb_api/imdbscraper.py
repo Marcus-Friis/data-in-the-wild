7d86fd8be28be3f0e50f7a813a5c5badd6e6e3f6
Marcus-Friis <marcus.friis99@gmail.com>
2022-12-19 02:02:19 +0100

    commented code

--- src/imdb_api/imdbscraper.py
+++ src/imdb_api/imdbscraper.py
@@ -6,13 +6,31 @@ import numpy as np
 
 
 class ImdbScraper:
+    """
+    A class for scraping movie release dates from IMDb
+    """
+
     def __init__(self):
+        """
+        Initialize the data attribute to store the tconst and release_date_us values
+        """
         self.data = {'id': [], 'release_date_us': []}
 
     def scrape_dates(self, tconsts, timeout=.1, verbose=False):
+        """
+        Scrape the release dates for the given tconsts (IMDb movie ID).
+        This method gets the first american date.
+        :param tconsts: A list of IMDb movie IDs (tconsts) to scrape release dates for
+        :param timeout: The time to sleep between requests. Defaults to 0.1.
+        :param verbose: If set to True, prints the tconst and release date for each movie. Defaults to False.
+        :return:
+        """
         for tconst in tconsts:
+            # get html response and parse it with BeautifulSoup
             response = requests.get(f'https://www.imdb.com/title/{tconst}/releaseinfo')
             soup = BeautifulSoup(response.text)
+
+            # loop through all tr and find the one corresponding to US, add it to the data and break
             trs = soup.find_all('tr')
             if verbose:
                 print(tconst)
@@ -28,14 +46,28 @@ class ImdbScraper:
             sleep(timeout)
 
     def scrape_dates_alternate(self, tconsts, timeout=.1, verbose=False):
+        """
+        Scrape the release dates for the given tconsts (IMDb movie ID).
+        This method gets the most frequent date on the IMDb website.
+        :param tconsts: A list of IMDb movie IDs (tconsts) to scrape release dates for
+        :param timeout: The time to sleep between requests. Defaults to 0.1.
+        :param verbose: If set to True, prints the tconst and release date for each movie. Defaults to False.
+        :return:
+        """
         for tconst in tconsts:
             if verbose:
                 print(tconst)
+
+            # get html response and parse it with BeautifulSoup
             response = requests.get(f'https://www.imdb.com/title/{tconst}/releaseinfo')
             soup = BeautifulSoup(response.text)
+
+            # find all dates and sort them in descending order
             dates = re.findall('\d{1,2} [A-z]+ \d{4}', str(soup))
             dates, counts = np.unique(dates, return_counts=True)
             idx = np.argsort(-counts)
+            # if there are any dates, pick the most common one
+            # if they are all equally frequent, pick the earliest date
             if dates.size > 0:
                 if counts.max() > 1:
                     release_date = dates[idx][0]
@@ -56,3 +88,4 @@ if __name__ == '__main__':
     scraper = ImdbScraper()
     scraper.scrape_dates_alternate(df.tconst.iloc[-10:], verbose=True)
     print(pd.DataFrame(scraper.data))
+
